6bb0c9b6c4def4f3a0162a03b08c12abca60fc4d
winston-lim <winston_lim1@hotmail.com>
2022-09-04 11:37:12 +0800

    Update style and compilation error fixes

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,14 +1,20 @@
 interface UserInterface {
     // Getters
     String getName();
+
     int getId();
+
     List<Item> getSoldItems();
+
     List<Item> getBoughtItems();
+
     List<Transaction> getTransactions();
 
     // Setters/Mutators
-    void addSoldItem() {}
-    void addBoughtItem() {}
-    void addTransaction() {}
+    void addSoldItem();
+    
+    void addBoughtItem();
+
+    void addTransaction();
 
 }
